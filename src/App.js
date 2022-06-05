e4a2c43c1bbe5c670caa14509ef6bcc724b7cc76
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-06-05 11:07:19 +0300

    delete butonunu yaptım

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       .then((data) => {
         setAppointmentList(data);
       });
-  });
+  }, []);
 
   useEffect(() => {
     fetchData();
@@ -29,7 +29,17 @@ function App() {
       <Search />
       <ul className="divide-y divide-gray-200">
         {appointmentList.map((appointment) => (
-          <AppointmentInfo key={appointment.id} appointment={appointment} />
+          <AppointmentInfo
+            key={appointment.id}
+            appointment={appointment}
+            onDeleteAppointment={(appointmentId) =>
+              setAppointmentList(
+                appointmentList.filter(
+                  (appointment) => appointment.id !== appointmentId
+                )
+              )
+            }
+          />
         ))}
       </ul>
     </div>
